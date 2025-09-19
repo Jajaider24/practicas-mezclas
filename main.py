5097e22c6c3689cd3b6621a7f96a68b60ef96deb
Jajaider24 <j41d3rr@gmail.com>
2025-09-19 08:21:06 -0500

    Hola hijueputas

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ print("Trabajen hijueputas")
 while True:
     print("chupones")
     break
+
+def es_mayor_de_edad(edad):
+        return edad >= 18
+
+edades = [15, 22, 17, 30, 19]
+
+for edad in edades:
+        if es_mayor_de_edad(edad):
+            print(f"La persona con edad {edad} es mayor de edad.")
+        else:
+            print(f"La persona con edad {edad} NO es mayor de edad.")
+
+
